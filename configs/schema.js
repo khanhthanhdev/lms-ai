fa3841b6fda8eaefaeb6b85061daccd61d06a3c6
khanhthanhdev <thanhkt27507@gmail.com>
2024-11-19 13:14:19 +0700

    add drizzle-orm neon, be setup

---
+++ configs/schema.js
@@ -0,0 +1,9 @@
+
+import { pgTable, varchar,serial, boolean } from "drizzle-orm/pg-core";
+
+export const USER_TABLE = pgTable('user', {
+    id:serial().primaryKey(),
+    name:varchar().notNull(),
+    email:varchar().notNull(),
+    isMember:boolean().default(false),
+})
